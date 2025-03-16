20356d6874f3c5bd4048dd89d28d30cdfce563f0
Michael Cappeller <mikec5799@gmail.com>
2025-03-16 13:22:37 -0400

    create mock powerpoint

--- functions/src/presentation/index.ts
+++ functions/src/presentation/index.ts
@@ -3,26 +3,43 @@ import {onDocumentCreated} from "firebase-functions/firestore";
 import {logger} from "firebase-functions";
 import {getFirestore} from "firebase-admin/firestore";
 import {Status} from "../types";
+import pptxgen from "pptxgenjs";
+import {getDownloadURL, getStorage} from "firebase-admin/storage";
 
 const firestore = getFirestore();
+const storage = getStorage();
 
-// eslint-disable-next-line max-len
 export const presentation = onDocumentCreated("company-profile/{symbol}", async (event) => {
-    const data = event.data;
     const symbol = event.params.symbol;
-    logger.info("new profile created", {symbol, data});
+    try {
+        const data = event.data;
+        logger.info("creating presentation", {symbol});
+        const name: string = data?.get("name");
+        const pres = buildPresentation(name);
 
-    // Wait 15 seconds and update the documents status to PresentationCreated
-    setTimeout(async () => {
-        try {
-            // Update the document status to Created to indicate that the presentation has been created
-            // and add a dummy url to the presentation
-            await firestore.doc(`company-profile/${symbol}`).update({status: Status.Created, downloadUrl: "https://example.com"});
-            // eslint-disable-next-line max-len
-            logger.info("profile status updated", {symbol, status: Status.Created});
-        } catch (error) {
-            // @ts-ignore
-            logger.error("error updating profile status", {error: error.message});
-        }
-    }, 15000);
+        // Save presentation to Cloud Storage
+        const presBlob = await pres.write({outputType: 'uint8array'}) as Uint8Array<ArrayBufferLike>;
+        const bucket = storage.bucket();
+        const file = bucket.file(`presentations/${symbol}.pptx`);
+        await file.save(presBlob);
+
+        // Get download url
+        const url = await getDownloadURL(file);
+
+        // Update Firestore document with download URL
+        await firestore.collection("company-profile").doc(symbol).update({
+            status: Status.Created,
+            downloadUrl: url
+        });
+    } catch (error) {
+        // @ts-ignore
+        logger.error("Error creating presentation for symbol", {symbol, error: error.message});
+    }
 });
+
+const buildPresentation = (name: string) => {
+    let pres = new pptxgen();
+    let slide = pres.addSlide();
+    slide.addText(name, {x: 1, y: 1, fontSize: 18, color: "363636"});
+    return pres;
+}
